e7f7c0c62d3af09362b6765e247b1ef820a82997
chrislind95 <christian.lindgren95@gmailcom>
2026-04-06 19:25:56 +0200

    Fitnesscenter link

---
+++ Presentation.WebbApp/Controllers/FitnessCentersController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.WebbApp.Controllers;
+
+[Route("fitness-centers")]
+public class FitnessCentersController : Controller
+{
+    [HttpGet("")]
+    [AllowAnonymous]
+    public IActionResult Index()
+    {
+        ViewData["Title"] = "Our Fitness Centers";
+        return View();
+    }
+}

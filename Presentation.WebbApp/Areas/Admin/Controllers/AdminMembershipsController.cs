84afb32a180b8c12ef7482ac8f9e5de0adca9c0b
chrislind95 <christian.lindgren95@gmailcom>
2026-04-17 13:40:42 +0200

    AdminPage started

---
+++ Presentation.WebbApp/Areas/Admin/Controllers/AdminMembershipsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.WebbApp.Areas.Admin.Controllers;
+
+[Area("Admin")]
+[Authorize(Roles = "Admin")]
+[Route("admin")]
+public class AdminMembershipsController : Controller
+{
+    [HttpGet("memberships")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
